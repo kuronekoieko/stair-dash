927223bb29ea0920ee9357eedde709446daafecd
家子智成 <>
2020-09-05 15:54:59 +0900

    走る+カメラ追従

--- Assets/0_MyAssets/Scripts/Game/CameraController.cs
+++ Assets/0_MyAssets/Scripts/Game/CameraController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-/// <summary>
-/// Unityで解像度に合わせて画面のサイズを自動調整する
-/// http://www.project-unknown.jp/entry/2017/01/05/212837
-/// </summary>
 public class CameraController : MonoBehaviour
 {
-    public static CameraController i;
+    PlayerController playerController;
+    Vector3 offset;
+    private void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
     void Start()
     {
-        if (i == null) i = this;
+        offset = transform.position - playerController.transform.position;
     }
 
     void Update()
     {
 
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = playerController.transform.position + offset;
     }
 }
---
+++ Assets/0_MyAssets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.forward * 10f;
+    }
+
+    void Update()
+    {
+
+    }
+}
