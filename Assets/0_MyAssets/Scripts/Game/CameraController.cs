98fe12f850e886031b72fe1419832c798068c8df
家子智成 <>
2020-09-05 17:28:45 +0900

    カメラ追従

--- Assets/0_MyAssets/Scripts/Game/CameraController.cs
+++ Assets/0_MyAssets/Scripts/Game/CameraController.cs
@@ -5,25 +5,5 @@ using DG.Tweening;
 
 public class CameraController : MonoBehaviour
 {
-    PlayerController playerController;
-    Vector3 offset;
-    private void Awake()
-    {
-        playerController = FindObjectOfType<PlayerController>();
-    }
 
-    void Start()
-    {
-        offset = transform.position - playerController.transform.position;
-    }
-
-    void Update()
-    {
-
-    }
-
-    private void LateUpdate()
-    {
-        transform.position = playerController.transform.position + offset;
-    }
 }
