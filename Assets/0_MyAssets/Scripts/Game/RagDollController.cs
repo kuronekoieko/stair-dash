040794566d625d52b3d828a04901eee7faadaae5
家子智成 <>
2020-09-06 21:02:08 +0900

    障害物の人作成

---
+++ Assets/0_MyAssets/Scripts/Game/RagDollController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    Rigidbody[] ragdollRbs;
+    private void Awake()
+    {
+        ragdollRbs = GetComponentsInChildren<Rigidbody>();
+    }
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (var rb in ragdollRbs)
+            {
+                var vec = transform.position - other.transform.position;
+                rb.AddForce(vec.normalized * 100f, ForceMode.Impulse);
+                animator.enabled = false;
+            }
+        }
+    }
+}
