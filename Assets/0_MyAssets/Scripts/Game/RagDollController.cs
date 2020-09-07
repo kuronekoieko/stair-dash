b5b3b137ede5f2a9f3d926f550ccfcdfa54e8116
家子智成 <>
2020-09-07 15:50:56 +0900

    歩く動作

--- Assets/0_MyAssets/Scripts/Game/RagDollController.cs
+++ Assets/0_MyAssets/Scripts/Game/RagDollController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public enum ObstacleType
 {
     Sitting = 0,
-    WalkWithPapers = 1,
+    WalkWithLuggages = 1,
 }
 public class RagDollController : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] ParticleSystem hitPS;
     [SerializeField] ObstacleType obstacleType;
+    [SerializeField] Rigidbody parentRb;
+
     Rigidbody[] ragdollRbs;
     private void Awake()
     {
@@ -19,6 +21,34 @@ public class RagDollController : MonoBehaviour
     }
     void Start()
     {
+        parentRb.isKinematic = false;
+        switch (obstacleType)
+        {
+            case ObstacleType.Sitting:
+                parentRb.isKinematic = true;
+                break;
+            case ObstacleType.WalkWithLuggages:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch (obstacleType)
+        {
+            case ObstacleType.Sitting:
+
+                break;
+            case ObstacleType.WalkWithLuggages:
+                var vel = parentRb.velocity;
+                vel.z = transform.forward.z * 2f;
+                parentRb.velocity = vel;
+                break;
+            default:
+                break;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,8 +61,10 @@ public class RagDollController : MonoBehaviour
         if (!other.gameObject.CompareTag("Player")) { return; }
         hitPS.transform.position = other.ClosestPoint(other.transform.position);
         hitPS.Play();
+        parentRb.isKinematic = true;
         foreach (var rb in ragdollRbs)
         {
+            rb.isKinematic = false;
             var vec = transform.position - other.transform.position;
             rb.AddForce(vec.normalized * 100f, ForceMode.Impulse);
             animator.enabled = false;
